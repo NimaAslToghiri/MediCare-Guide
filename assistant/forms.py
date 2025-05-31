3c74ba64d7a3dfdb2949c6afe31a567e52074ad4
Nima <87418027+NimaAslToghiri@users.noreply.github.com>
2025-05-31 14:12:01 +0330

    Delete assistant directory

--- assistant/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import MedicalDocument
-
-class MedicalDocumentForm(forms.ModelForm):
-    class Meta:
-        model = MedicalDocument
-        fields = ['file']
